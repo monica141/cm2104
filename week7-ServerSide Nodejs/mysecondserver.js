537fd0134437a3fdd5c16c2fc05a35cd7b71fde0
Monica Dayalani <monicadayalani@hotmail.com>
2018-04-15 03:04:37 +0000

    corrected

--- week7-ServerSide Nodejs/mysecondserver.js
+++ week7-ServerSide Nodejs/mysecondserver.js
@@ -1,15 +1,4 @@
 var http = require('http');
-http.createServer(function (req, res) {
- res.writeHead(200, {
-    'Content-Type': 'text/html'
-});
- res.end('Hello World!');
-})
-.listen(8080);
-
-//This tells node to use our new module
-//and then our myDateTime function
-var http = require('http');
 var currentdate = require('./mymodule');
 http.createServer(function (req, res) {
  res.writeHead(200, {'Content-Type': 'text/html'});
